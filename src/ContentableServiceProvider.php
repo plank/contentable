e4b2833185b50aa40c9925819a54cd6e11d86fd1
Massimo Triassi <massimo@plankdesign.com>
2023-08-18 11:58:54 -0400

    feat: #1 automatically negotiate relationships at boot time

--- src/ContentableServiceProvider.php
+++ src/ContentableServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Plank\Contentable;
 
+use Illuminate\Database\Eloquent\Model;
+use Plank\Contentable\Contracts\ContentInterface;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Plank\Contentable\Commands\ContentableCommand;
@@ -10,16 +12,33 @@ class ContentableServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('contentable')
             ->hasConfigFile()
             ->hasViews()
             ->hasMigration('create_contents_table')
             ->hasCommand(ContentableCommand::class);
+    }
+
+    public function packageBooted()
+    {
+
+        \Plank\Contentable\Facades\Contentable::discover();
+        $contents = config('contentable.contents');
+        $contentables = config('contentable.contentables');
+
+        // create the relationships on the fly / at runtime
+        // https://laravel.com/docs/10.x/eloquent-relationships#dynamic-relationships
+        foreach ($contents as $content) {
+            foreach ($contentables as $contentable) {
+                $content::resolveRelationUsing('contentable', function ($content) use ($contentable) {
+                    return $content->morphedByMany($contentable, 'contentable', 'contents', 'content_id');
+                });
+                $contentable::resolveRelationUsing('content', function ($contentable) use ($content) {
+                   return $contentable->morphedByMany($content, 'content', 'contents', 'contentable_id');
+                });
+            }
+        }
+
     }
 }

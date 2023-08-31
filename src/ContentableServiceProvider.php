a7deae5f54451dd6239acb96547d43701db37c3e
Massimo Triassi <massimo@plankdesign.com>
2023-08-31 15:53:30 -0400

    chore: #1 cleanup unneeded code since relations arent runtime defined

--- src/ContentableServiceProvider.php
+++ src/ContentableServiceProvider.php
@@ -19,28 +19,4 @@ class ContentableServiceProvider extends PackageServiceProvider
             ->hasMigration('create_contents_table')
             ->hasCommand(ContentableCommand::class);
     }
-
-    public function packageBooted()
-    {
-
-        \Plank\Contentable\Facades\Contentable::discover();
-        $contents = config('contentable.contents');
-        $contentables = config('contentable.contentables');
-
-        // create the relationships on the fly / at runtime
-        // https://laravel.com/docs/10.x/eloquent-relationships#dynamic-relationships
-        foreach ($contents as $content) {
-            foreach ($contentables as $contentable) {
-                $relationContent = str($content)->afterLast("\\")->lower()->plural();
-                $relationContentable = str($contentable)->afterLast("\\")->lower()->plural();
-                $content::resolveRelationUsing('contentable', function ($content) use ($contentable) {
-                    return $content->morphedByMany($contentable, 'contentable', 'contentables', 'content_id');
-                });
-                $contentable::resolveRelationUsing('content', function ($contentable) use ($content) {
-                   return $contentable->morphedByMany($content, 'content', 'contentables', 'contentable_id');
-                });
-            }
-        }
-
-    }
 }

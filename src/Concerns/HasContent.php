6cd5d7172d81c03cb5665060e50981a55bc1ff3b
Massimo Triassi <massimo@plankdesign.com>
2023-08-31 16:46:27 -0400

    feat: #1 add render-ability to trait so page can render full content

--- src/Concerns/HasContent.php
+++ src/Concerns/HasContent.php
@@ -2,12 +2,73 @@
 
 namespace Plank\Contentable\Concerns;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Facades\Cache;
 use Plank\Contentable\Contracts\ContentInterface;
+use Plank\Contentable\Contracts\RenderableInterface;
+use Plank\Contentable\Models\Content;
 
 trait HasContent
 {
-    public function attachContent(ContentInterface $content)
+    public function contents(): MorphMany
     {
-        return $this->content()->attach($content, ['contentable_type' => self::class]);
+        return $this->morphMany(Content::class, 'contentable');
+    }
+
+    public function scopeInRenderableOrder(Builder $q): void
+    {
+        $q->orderBy('id');
+    }
+
+    public function attachContent(RenderableInterface $renderable, $identifier = null)
+    {
+        if (Cache::has("contentable.html.{$this->id}")) {
+            Cache::delete("contentable.html.{$this->id}");
+        }
+
+        if (Cache::has("contentable.json.{$this->id}")) {
+            Cache::delete("contentable.json.{$this->id}");
+        }
+
+        return $this->contents()->create(array_merge([
+            'renderable_type' => $renderable::class,
+            'renderable_id' => $renderable->id
+        ], $identifier ? ['identifier' => $identifier] : []));
+    }
+
+    public function renderHtml(): string
+    {
+        if (Cache::has("contentable.html.{$this->id}")) {
+            return Cache::get("contentable.html.{$this->id}");
+        }
+
+        $output = "";
+        foreach ($this->contents as $content) {
+            $output .= $content->renderable->renderHtml() . "\n";
+        }
+
+        Cache::put("contentable.html.{$this->id}", $output, 10800);
+
+        return $output;
+    }
+
+    public function renderJson(): string
+    {
+        if (Cache::has("contentable.json.{$this->id}")) {
+            return Cache::get("contentable.json.{$this->id}");
+        }
+
+        $output = [];
+
+        foreach ($this->contents as $content) {
+            $output[] = $content->renderable->renderJson();
+        }
+
+        $output = json_encode($output);
+
+        Cache::put("contentable.json.{$this->id}", $output, 10800);
+
+        return $output;
     }
 }
--- src/Contracts/ContentableInterface.php
+++ src/Contracts/ContentableInterface.php
@@ -2,7 +2,18 @@
 
 namespace Plank\Contentable\Contracts;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
 interface ContentableInterface
 {
-    public function attachContent(ContentInterface $content);
+    public function contents(): MorphMany;
+
+    public function scopeInRenderableOrder(Builder $q): void;
+
+    public function attachContent(RenderableInterface $renderable);
+
+    public function renderHtml(): string;
+
+    public function renderJson(): string;
 }

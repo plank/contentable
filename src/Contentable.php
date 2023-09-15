c8f3eb7674b928df0ea8f29858695251c446e3ab
Massimo Triassi <massimo@plankdesign.com>
2023-09-15 12:50:33 -0400

    refactor: #1 extract cache busting to globally accessible function on base class

--- src/Contentable.php
+++ src/Contentable.php
@@ -2,7 +2,18 @@
 
 namespace Plank\Contentable;
 
+use Illuminate\Support\Facades\Cache;
+
 class Contentable
 {
+    public function clearCache($key)
+    {
+        if (Cache::has("contentable.html.{$key}")) {
+            Cache::delete("contentable.html.{$key}");
+        }
 
+        if (Cache::has("contentable.json.{$key}")) {
+            Cache::delete("contentable.json.{$key}");
+        }
+    }
 }

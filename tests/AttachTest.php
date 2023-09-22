bb584ab78997d392e3d1d76034b748a8e511132e
Massimo Triassi <massimo@plankdesign.com>
2023-09-22 11:53:40 -0400

    test: #1 add test for syncContent

--- tests/AttachTest.php
+++ tests/AttachTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Plank\Contentable\Models\Content;
 use Plank\Contentable\Tests\Models\FakeModule;
 use Plank\Contentable\Tests\Models\Page;
 
@@ -17,4 +18,39 @@ it('can attach new renderables to a piece of content', function () {
     expect($module->renderable)->toEqual($renderable->fresh());
 });
 
+
+it('can sync new renderables to a piece of content', function () {
+    $page = Page::factory()->create();
+    $renderables = FakeModule::factory(2)->create();
+
+    Content::create([
+        'contentable_id' => $page->id,
+        'contentable_type' => $page::class,
+        'renderable_id' => $renderables[0]->id,
+        'renderable_type' => $renderables[0]::class
+    ]);
+
+    Content::create([
+        'contentable_id' => $page->id,
+        'contentable_type' => $page::class,
+        'renderable_id' => $renderables[1]->id,
+        'renderable_type' => $renderables[1]::class
+    ]);
+
+    // add 2 new modules, keep the first module, but de-sync the second module
+    // ids: 1, 3, 4
+    $sync = FakeModule::factory(2)->create()->merge([$renderables[0]]);
+
+    $page->syncContent($sync);
+
+    $attached = $page->contents->pluck('renderable_type', 'renderable_id')->all();
+    $expected = [
+        1 => FakeModule::class,
+        3 => FakeModule::class,
+        4 => FakeModule::class
+    ];
+
+    expect($attached)->toEqual($expected);
+
+});
 

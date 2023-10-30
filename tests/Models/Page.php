a3f81bdc5305844375c977e059c27cd3ffb245c5
Massimo Triassi <massimo@plankdesign.com>
2023-10-30 16:52:31 -0400

    test: #1 add missing interface reference to fake page model

--- tests/Models/Page.php
+++ tests/Models/Page.php
@@ -5,9 +5,10 @@ namespace Plank\Contentable\Tests\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Plank\Contentable\Concerns\HasContent;
+use Plank\Contentable\Contracts\ContentableInterface;
 use Plank\Contentable\Tests\Factories\PageFactory;
 
-class Page extends Model
+class Page extends Model implements ContentableInterface
 {
     use HasFactory;
     use HasContent;

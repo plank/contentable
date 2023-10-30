4215a9ef89a5ce31043f160dd09c282b0b4242f9
Massimo Triassi <massimo@plankdesign.com>
2023-10-30 17:16:31 -0400

    docs: #1 add non-obvious properties to interaface for IDE autocomplete

--- src/Contracts/Contentable.php
+++ src/Contracts/Contentable.php
@@ -5,6 +5,9 @@ namespace Plank\Contentable\Contracts;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
+/**
+ * @property string $render_order_column
+ */
 interface Contentable
 {
     public function contents(): MorphMany;

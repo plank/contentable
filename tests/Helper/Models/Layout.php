5ffd8a1133df494c4bb92ca52696441f8eadbcae
Kurt Friars <kfriars@apple.com>
2023-11-01 02:20:28 +0800

    Strip deprecated menu functions from test layout

--- tests/Helper/Models/Layout.php
+++ tests/Helper/Models/Layout.php
@@ -3,7 +3,6 @@
 namespace Plank\Contentable\Tests\Helper\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Plank\Contentable\Models\Layout as PackageLayout;
 
 class Layout extends PackageLayout
@@ -17,17 +16,4 @@ class Layout extends PackageLayout
     protected $casts = [
         'meta' => 'json',
     ];
-
-    public function menus(): BelongsToMany
-    {
-        return $this->belongsToMany(Menu::class)
-            ->withPivot('key');
-    }
-
-    public function menu(string $key): ?Menu
-    {
-        return $this->menus()
-            ->wherePivot('key', $key)
-            ->first();
-    }
 }

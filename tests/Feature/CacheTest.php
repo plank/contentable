95a5c6b28b8645a350c33b58b384f8b7aeb365ca
Massimo Triassi <massimo@plankdesign.com>
2023-11-02 15:01:55 -0400

    test: #1 adjust test to expect modules attached to be attached to 1 contentable

--- tests/Feature/CacheTest.php
+++ tests/Feature/CacheTest.php
@@ -35,28 +35,20 @@ it('clears the cached json when its content changes', function () {
 });
 
 it('clears cache if a renderable attached to a contentable is updated', function () {
-    $page1 = Page::factory()->create();
-    $page2 = Page::factory()->create();
+    $page = Page::factory()->create();
     $renderable = FakeModule::factory()->create();
-    $page1->attachContent($renderable);
-    $page2->attachContent($renderable);
+    $page->attachContent($renderable);
 
-    $page1->renderHtml();
-    $page1->renderJson();
-    $page2->renderHtml();
-    $page2->renderJson();
+    $page->renderHtml();
+    $page->renderJson();
 
-    expect(Cache::has("contentable.json.{$page1->id}"))->toBeTrue();
-    expect(Cache::has("contentable.json.{$page2->id}"))->toBeTrue();
-    expect(Cache::has("contentable.html.{$page1->id}"))->toBeTrue();
-    expect(Cache::has("contentable.html.{$page2->id}"))->toBeTrue();
+    expect(Cache::has("contentable.json.{$page->id}"))->toBeTrue();
+    expect(Cache::has("contentable.html.{$page->id}"))->toBeTrue();
 
     $renderable->title = 'New Title!';
     $renderable->save();
 
-    expect(Cache::has("contentable.json.{$page1->id}"))->toBeFalse();
-    expect(Cache::has("contentable.json.{$page2->id}"))->toBeFalse();
-    expect(Cache::has("contentable.html.{$page1->id}"))->toBeFalse();
-    expect(Cache::has("contentable.html.{$page2->id}"))->toBeFalse();
+    expect(Cache::has("contentable.json.{$page->id}"))->toBeFalse();
+    expect(Cache::has("contentable.html.{$page->id}"))->toBeFalse();
 
 });
